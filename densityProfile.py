ed8ce8c26794b741c30c794ea0805161e459db6a
Tanooj Shah <tanooj.s.shah@gmail.com>
2025-05-26 13:32:26 -0700

    cleaned up 1d density profile script

--- densityProfile.py
+++ densityProfile.py
@@ -1,9 +1,4 @@
-# read in a dump file, calculate z-resolved density profiles of specified atom types
-# this version for large dump files - do analysis while file is being read in
-# user needs to specify minz, maxz and start timestep for analysis here
 
-# (useful for interfaces, responses to external potentials etc)
-# output a numpy array of shape (1+nTypes, nbins)
 
 import argparse
 import numpy as np
@@ -11,20 +6,16 @@ from tqdm import tqdm
 import time
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(description="calculate density profiles")
+parser = argparse.ArgumentParser(description="calculate 1d density profiles")
 parser.add_argument('-i', action="store", dest="input")
 parser.add_argument('-dz', action="store", dest="dz") # bin width 
-parser.add_argument('-minz', action="store", dest="minz")
-parser.add_argument('-maxz', action="store", dest="maxz")
 parser.add_argument('-start', action="store", dest="start")
-parser.add_argument('-t', action="store", dest="atom_types") # atom types to calculate profiles for, string like '1 3 4'
 args = parser.parse_args()
 
+atom_types = [1,2,3] # edit this as appropriate 
+
 dz = float(args.dz)
-minz = float(args.minz)
-maxz = float(args.maxz)
 start = int(args.start)
-atom_types = [int(t) for t in args.atom_types.split(' ')]
 print(f'Atom types to bin: {atom_types}')
 
 def purge(tokens): return [t for t in tokens if len(t) >= 1] # purge empty strings from token lists
@@ -37,13 +28,24 @@ def isfloat(s): # hack function to confirm tokens are numeric as floats
 		return False
 
 # ---- single pass over file ----
-print(f'MinZ: {minz}')
-print(f'MaxZ: {maxz}')
-nBins = int((maxz-minz)/dz) + 1
-rho = np.zeros((1+len(atom_types), nBins)) # output array
+# edit dimensions as appropriate for other directions 
+minz, maxz = 0, 0
+with open(args.input,'r') as f:
+	nline = 0
+	for line in f:
+		tokens = purge(line.strip('\n').split(' '))
+		print(tokens)
+		if nline == 7: 
+			minz, maxz = float(tokens[0]), float(tokens[1])
+			break
+		nline += 1
+
+print(f"minz: {minz}")
+print(f"maxz: {maxz}")
+
+nbins = int((maxz-minz)/dz) + 1
+rho = np.zeros((1+len(atom_types), nbins)) # output array
 # print out which atom type is which row in output array
-for i1, i2 in enumerate(atom_types):
-	print(f'Row {i1+1} | Atom type {i2}') # 0 will be zs
 
 # last is species blind
 
@@ -55,20 +57,13 @@ with open(args.input,'r') as f:
 		line = line.strip('\n')
 		if line.startswith("ITEM: ATOMS"):
 			tokens = purge(line.split(' '))
-			print(tokens)
 			idIdx = tokens.index('id') - 2
 			typeIdx = tokens.index('type') - 2
 			xIdx = tokens.index('x') - 2
 			yIdx = tokens.index('y') - 2
 			zIdx = tokens.index('z') - 2
-print(f'{idIdx} {typeIdx} {xIdx} {yIdx} {zIdx}')
 
-
-# add mean field normalization
-# which is just the average number of particles within a dz bin (i.e. an x-y thin slab)
-
-
-print("Parsing and analyzing dump directly to calculate rho...")
+print("Parsing and analyzing dump to calculate rho...")
 timestart = time.time()
 with open(args.input,'r') as f:
 	nCollected = 0 # timesteps summed over
@@ -79,9 +74,8 @@ with open(args.input,'r') as f:
 		lines = line.strip('\n')
 		tokens = purge(line.split(' '))
 		if doCollect == True:
-			# you only want to collect data when all tokens are numeric 
 			checksum = np.sum([not isfloat(t) for t in tokens])
-			if (len(tokens) > 4) and (checksum == 0):
+			if (len(tokens) > 2) and (checksum == 0):
 				# bin data here 
 				aType, z = int(tokens[typeIdx]), float(tokens[zIdx])
 				if (aType in atom_types) and (z >= minz) and (z <= maxz):
@@ -96,12 +90,13 @@ with open(args.input,'r') as f:
 			currentTime = int(tokens[0])
 		previousLine = line
 print(f'{round((time.time()-timestart)/60,4)} minutes')
-rho /= nCollected
-print(rho.shape)
 print(f'{nCollected} timesteps collected')
 
+rho /= nCollected
+print(rho.shape)
+
 # append z values (i.e. x axis of histograms) as first row
-zs = np.arange(minz,maxz+0.1*dz,dz) # hack
+zs = minz + dz*np.arange(0,rho.shape[1],1)
 print(zs.shape)
 assert len(zs) == rho.shape[1]
 zs = np.reshape(zs, (1, rho.shape[1]))
@@ -114,3 +109,7 @@ N = np.tile(N,rho.shape[1])
 rho[1:] /= N
 
 with open(args.input[:-5]+".rho_z.npy",'wb') as f: np.save(f, rho)
+# assuming input file is named .dump and not .dump.0
+
+
+
