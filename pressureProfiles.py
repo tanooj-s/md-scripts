7a21f0f7b9ed3a80a62677e6907d2fa62d25e0fd
Tanooj Shah <tanooj.s.shah@gmail.com>
2026-03-13 11:58:22 -0400

    removed comments

--- pressureProfiles.py
+++ pressureProfiles.py
@@ -10,9 +10,8 @@ import matplotlib.pyplot as plt
 inputfile = sys.argv[1]
 dz = float(sys.argv[2])
 zlo = float(sys.argv[3])
-zhi = float(sys.argv[4]) # these you need to rationally set from the dump file in ovito 
+zhi = float(sys.argv[4])
 
-# unit conversion factors
 eV_per_cubic_angstrom_to_gigapascal = 160.2176634 
 
 # ---------------
@@ -43,10 +42,6 @@ def isfloat(s):
 
 # two passes 
 # compute trajectory of box bounds, atom_data in first pass
-# then bin data afterwards 
-# you can just keep appending and reshape afterwards
-# wtf
-# all my old code was so shit 
 
 
 timesteps = []
@@ -55,7 +50,6 @@ bounds = []
 # timeseries of profiles
 atom_data = [] # atom_data reshaping only works if N is constant 
 
-# timeseries of (3,2) shaped objects 
 with open(inputfile,'r') as f:
 	previousline = ""
 	for line in f:
@@ -79,25 +73,19 @@ bounds = np.array(bounds)
 atom_data = np.array(atom_data)
 natoms = np.array(natoms) 
 
-# bin separately 
-N = natoms[0] # this isn't changing in regular sims
+N = natoms[0]
 bounds = bounds.reshape((timesteps.shape[0],int(bounds.shape[0]/timesteps.shape[0]),2))
 atom_data = atom_data.reshape((timesteps.shape[0],int(atom_data.shape[0]/timesteps.shape[0]),atom_data.shape[1]))
 print(bounds.shape)
 print(atom_data.shape)
 assert bounds.shape[0] == atom_data.shape[0]
 
-
-
-# now actually compute profiles 
 nbins = int((zhi-zlo)/dz) + 1
-# histogram arrays, implicitly as a function of time averaged later
 counts_z = []
 Pzz_z = []
 Pxx_z = []
 Pyy_z = []
 
-# collect data 
 for t in range(bounds.shape[0]):
 	xlo = bounds[t][0][0]
 	xhi = bounds[t][0][1]
@@ -138,35 +126,22 @@ for t in range(bounds.shape[0]):
 	Pyy_z.append(Pyy_zt)
 
 
-# cast and convert cubic angstrom to gigapascal
 Pzz_z = np.array(Pzz_z) * eV_per_cubic_angstrom_to_gigapascal
 Pxx_z = np.array(Pxx_z) * eV_per_cubic_angstrom_to_gigapascal
 Pyy_z = np.array(Pyy_z) * eV_per_cubic_angstrom_to_gigapascal
-
-print(Pzz_z.shape)
-print(Pxx_z.shape)
-print(Pyy_z.shape)
 
 Pzz_z = np.mean(Pzz_z,axis=0)
 Pxx_z = np.mean(Pxx_z,axis=0)
 Pyy_z = np.mean(Pyy_z,axis=0)
 
-
-# convert to normal and tangential 
 Pn_z = Pzz_z
 Pt_z = 0.5*(Pxx_z+Pyy_z)
 
-
-# moving average to plot out 
-# denoise
 moving_avg_wdw = 20
 Pn_z = moving_avg(Pn_z,moving_avg_wdw)
 Pt_z = moving_avg(Pt_z,moving_avg_wdw)
 z = dz*np.arange(0,Pn_z.shape[0],1)
 
-
-
-# plot for debugging
 
 plt.rcParams["figure.figsize"] = (8,9)
 fig, axes = plt.subplots(3,1,sharex=True)
@@ -178,9 +153,4 @@ plt.legend(loc="upper right")
 plt.savefig("pressure_profile_test.png")
 
 exit()
-
-
-# clearly glass interface sims are weird so need to confirm whether this is a valid strategy at all for gamma incorporating entropy
-# but should be valid for vacuum sims 
-# so I guess what I'm seeing is a nonzero pressure in the glass, but close to zero for CoSi
 
